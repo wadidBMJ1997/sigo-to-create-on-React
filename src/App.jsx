769bf539aa416d8c4a409eed2e32b2e5a6205cf8
Wadid Castilla <wadidcastillab@gmail.com>
2024-05-22 11:09:25 -0500

    initation of proyect

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from './components/Hello'
+
+function App() {
+  return (
+    <>
+      <Hello/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Hello =()=>{
+    return(
+        <h1>Hello word</h1>
+    )
+}
+
+export default Hello;
+
